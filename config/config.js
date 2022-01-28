09f14f5e43b16b84f019051c4da1fdbe2d59a726
Murilo Rainho <murilorainho01@gmail.com>
2022-01-28 08:38:02 -0300

    fix config.js sequelize

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  host: process.env.HOSTNAME,
+  host: process.env.MYSQL_HOST,
   database: 'StoreManager',
   dialect: 'mysql',
 };
